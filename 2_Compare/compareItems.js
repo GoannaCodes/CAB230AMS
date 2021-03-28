eca1ddb3b2cdfc94977517fb59105a6c2b0f633a
GoannaCodes <anguy100@hotmail.com>
2021-03-28 23:13:47 +1000

    Updated solution to compareItems

--- 2_Compare/compareItems.js
+++ 2_Compare/compareItems.js
@@ -1,1 +1,15 @@
+/*
+Requirements:
+Write a function compare which returns the boolean result of its two 
+parameters by checking that they have equal value and equal type.
 
+Use the following provided code to begin:
+
+function compare(x, y) {
+    
+}
+*/
+
+function compare(x, y){
+    return (x === y);
+}
